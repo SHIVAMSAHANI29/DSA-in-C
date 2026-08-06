6a88f566125c359eadc9c7e69b7d0f980bd32154
SHIVAM SAHANI <shivamsahaniddu2507@gmail.com>
2026-08-06 23:58:41 +0530

    Initial commit

--- linkedlist.c
+++ linkedlist.c
@@ -43,37 +43,3 @@ printf("NULL\n");
 
     return 0;
 }
-
-// #include<stdio.h>
-// #include<stdlib.h>
-
-// struct Node{
-//     int data;
-//     struct Node * next ;
-//     };
-    
-//   struct Node *head = NULL;
-// void insertEnd(int value)
-// {
-//     struct Node *newNode, *temp;
-
-//     newNode = (struct Node *)malloc(sizeof(struct Node));
-
-//     newNode->data = value;
-//     newNode->next = NULL;
-
-//     if(head == NULL)
-//     {
-//         head = newNode;
-//         return;
-//     }
-
-//     temp = head;
-
-//     while(temp->next != NULL)
-//     {
-//         temp = temp->next;
-//     }
-
-//     temp->next = newNode;
-// }
