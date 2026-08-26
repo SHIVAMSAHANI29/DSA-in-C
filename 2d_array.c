9d47a90391f066a24583b80f4fa4f69bf6a93841
SHIVAM SAHANI <shivamsahaniddu2507@gmail.com>
2026-08-26 20:42:36 +0530

    Add DSA programs in C

--- 2d_array.c
+++ 2d_array.c
@@ -2,13 +2,12 @@
 int main()
 {
     int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-
-     // Outer loop for rows
-    for (int i = 0; i < 3; i++) {
+      // Outer loop for rows
+    for (int i = 0; i < 2; i++) {
         // Inner loop for columns
         for (int j = 0; j < 3; j++) {
-
-    printf("Matrix = %d\n ",matrix[i][j]);
+          
+            printf("Matrix = %d\n ",matrix[i][j]);
         }
         printf("\n");
     }
--- Insertion_at_begining_ssl.c
+++ Insertion_at_begining_ssl.c
@@ -2,5 +2,17 @@
 #include<stdlib.h>
 
 struct Node{
-    struct Node * 
+    int data;
+    struct Node * next;
+
+};
+struct Node*head = NULL;
+//function to insert at begining
+void Insert_Begining(int value){
+    struct Node* newNode;
+    newNode = (struct Node *)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->next = head;
+    head = newNode;
 }
